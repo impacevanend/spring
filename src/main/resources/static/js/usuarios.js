79d87fc463d54b3926399d51cc5187c39188a70f
José Luis Bravo Carrillo <jose.bravo@novatec.loc>
2022-01-20 10:20:06 -0500

    modificando vista

---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,23 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    cargarUsuarios();
+  $('#usuarios').DataTable();
+});
+
+function cargarUsuarios(){
+
+(async () => {
+  const rawResponse = await fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 'Textual content'})
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
+})();
+
+}
