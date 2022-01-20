3619a8fd23dcebf1a1fe51e0cf08bd68b3729e59
José Luis Bravo Carrillo <jose.bravo@novatec.loc>
2022-01-20 17:57:56 -0500

    se agrega funcional front/back GET DELETE

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -3,8 +3,10 @@ package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,7 +15,11 @@ import java.util.List;
 @RestController
 public class UsuarioController  {
 
-    @RequestMapping(value = "usuario/{id}")
+    //Inyección de dependencia
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id){
             Usuario usuario = new Usuario();
             usuario.setId(id);
@@ -26,37 +32,13 @@ public class UsuarioController  {
 
 
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(234L);
-        usuario.setNombre("Jose");
-        usuario.setApellido("Bravo");
-        usuario.setEmail("jose@mail.com");
-        usuario.setTelefono("12321412");
+       return usuarioDao.getUsuarios();
 
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(567L);
-        usuario2.setNombre("Maria");
-        usuario2.setApellido("Gonzales");
-        usuario2.setEmail("maraigo@mail.com");
-        usuario2.setTelefono("54542534");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(456L);
-        usuario3.setNombre("Grabriel");
-        usuario3.setApellido("Aliaga");
-        usuario3.setEmail("alaiga124@mail.com");
-        usuario3.setTelefono("4546546");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return  usuarios;
     }
 
-    @RequestMapping(value = "usuario12")
+    @RequestMapping(value = "api/usuario12")
     public Usuario editar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Jose");
@@ -66,17 +48,14 @@ public class UsuarioController  {
         return usuario;
     }
 
-    @RequestMapping(value = "usuario34")
-    public Usuario eliminar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Jose");
-        usuario.setApellido("Bravo");
-        usuario.setEmail("jose@mail.com");
-        usuario.setTelefono("12321412");
-        return usuario;
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id){
+
+        usuarioDao.eliminar(id);
+
     }
 
-    @RequestMapping(value = "usuario56")
+    @RequestMapping(value = "api/usuario56")
     public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Jose");
